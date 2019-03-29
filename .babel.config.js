c9e64df86b4f2fa977100ad2cc56259963370053
Aaron Zemetres <azemetre@gmail.com>
2019-03-29 00:13:07 -0400

    build: setting up babel typescript config

---
+++ .babel.config.js
@@ -0,0 +1,19 @@
+module.exports = function (api) {
+    api.cache(true);
+
+    const presets = [
+        '@babel/preset-env',
+        '@babel/preset-react',
+        '@babel/preset-typescript',
+    ];
+
+    const plugins = [
+        '@babel/proposal-class-properties',
+        '@babel/proposal-object-rest-spread',
+    ];
+
+    return {
+        presets,
+        plugins,
+    };
+};
